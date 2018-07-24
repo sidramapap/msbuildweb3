fa9983cfecd00a97fbf0fb4dc6b8b6203ac71b3a
siddu <siddugulbarga011@gmail.com>
2018-07-24 15:18:10 +0530

    initial changes

---
+++ Webmsbuild3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webmsbuild3.MvcApplication" Language="C#" %>
---
+++ Webmsbuild3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webmsbuild3.Startup))]
+namespace Webmsbuild3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
